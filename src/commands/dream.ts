bac587b8614103be27df39e86b8cbdb8bb6a094e
Konstantin Akhanshchikov <k.akhanshchikov@planado.app>
2023-07-04 19:08:29 +0300

    Make retry button

--- src/commands/dream.ts
+++ src/commands/dream.ts
@@ -1,50 +1,149 @@
 import {
+  ActionRowBuilder,
   ApplicationCommandOptionType,
   CommandInteraction,
   EmbedBuilder,
-  InteractionReplyOptions,
+  ButtonBuilder,
+  ButtonInteraction,
+  MessageActionRowComponentBuilder,
+  ButtonStyle,
+  ComponentType,
   Locale,
 } from 'discord.js'
-import { Discord, Slash, SlashOption } from 'discordx'
+import { Discord, Slash, SlashOption, ButtonComponent } from 'discordx'
 import { Pulse } from '../types/pulse.js'
 import Dreamer from '../dreamer.js'
 import silent from '../utils/silent.js'
 import t from '../utils/t.js'
 
-async function embed(
-  imageUrl: string,
-  prompt: string,
-  locale: Locale,
-): Promise<EmbedBuilder> {
+const buttons = (
+  imageUrl: string, showRedoButton: boolean, locale: Locale) => {
+  const downloadButton = new ButtonBuilder()
+    .setLabel(t('dream.download', locale))
+    .setStyle(ButtonStyle.Link)
+    .setURL(imageUrl)
+
   return (
-    new EmbedBuilder()
-      .setTitle(t('dream.result', locale))
-      .setDescription(`${prompt}\n[${t('dream.download', locale)}](${imageUrl})`)
-      .setImage(imageUrl)
+    new ActionRowBuilder<MessageActionRowComponentBuilder>()
+      .addComponents(
+        showRedoButton ? [
+          downloadButton,
+          new ButtonBuilder()
+            .setLabel(t('dream.redo', locale))
+            .setStyle(ButtonStyle.Secondary)
+            .setCustomId('redo')
+        ] : [
+          downloadButton
+        ]
+      )
   )
 }
 
-const updateDisplayedImage = async (
+async function imageEmbed(
+  imageUrl: string,
+  prompt: string,
+  locale: Locale
+): Promise<EmbedBuilder> {
+  return new EmbedBuilder()
+    .setTitle(t('dream.result', locale))
+    .setDescription(prompt)
+    .setImage(imageUrl)
+}
+
+const failEmbed = (locale: Locale): EmbedBuilder =>
+  new EmbedBuilder().setTitle(t('dream.failed', locale))
+
+const updateDisplayedMessage = async (
   pulse: Pulse,
-  paramsWrapper: InteractionReplyOptions,
-  interaction: CommandInteraction,
+  beSilent: boolean | undefined,
+  interaction: CommandInteraction | ButtonInteraction,
 ) => {
   const mbResult = pulse.result?.final
   if (pulse.state === 'failed') {
     await interaction.editReply({
-      ...paramsWrapper,
-      embeds: [new EmbedBuilder().setTitle(t('dream.failed', interaction.locale))]
+      embeds: [failEmbed(interaction.locale)],
+      components: []
     })
   } else if (mbResult) {
-    await interaction.editReply({
-      ...paramsWrapper,
-      embeds: [await embed(mbResult, pulse.inputSpec.prompt, interaction.locale)]
+    const message = await interaction.editReply({
+      embeds: [await imageEmbed(mbResult, pulse.inputSpec.prompt, interaction.locale)],
+      components: [buttons(mbResult, true, interaction.locale)]
     })
+
+    try {
+      const buttonInteraction = await message.awaitMessageComponent({
+        componentType: ComponentType.Button, time: 30000
+      })
+
+      await interaction.editReply({
+        embeds: [await imageEmbed(mbResult, pulse.inputSpec.prompt, interaction.locale)],
+        components: [buttons(mbResult, false, interaction.locale)]
+      })
+
+      if (!buttonInteraction.deferred) {
+        if (beSilent) {
+          await buttonInteraction.deferReply(silent({}))
+        } else {
+          await buttonInteraction.deferReply()
+        }
+      }
+
+      await buttonInteraction.editReply({
+        embeds: [new EmbedBuilder().setTitle(t('dream.thinking', interaction.locale))],
+        components: []
+      })
+
+      await processOne(
+        pulse.inputSpec.prompt,
+        pulse.inputSpec.style,
+        beSilent,
+        buttonInteraction
+      )
+    } catch (e: any) {
+      if (e.toString().includes('Collector received no interactions before ending')) {
+        await interaction.editReply({
+          embeds: [await imageEmbed(mbResult, pulse.inputSpec.prompt, interaction.locale)],
+          components: [buttons(mbResult, false, interaction.locale)]
+        })
+      } else {
+        throw e
+      }
+    }
+  } else {
+    const lastPreview = pulse.photoUrlList.at(-1)
+
+    if (lastPreview) {
+      await interaction.editReply({
+        embeds: [await imageEmbed(lastPreview, pulse.inputSpec.prompt, interaction.locale)]
+      })
+    }
   }
+}
+
+async function processOne(
+  prompt: string,
+  style: number | undefined,
+  beSilent: boolean | undefined,
+  interaction: CommandInteraction | ButtonInteraction
+): Promise<any> {
+  const finalPulse =
+    await Dreamer.dream(
+      style,
+      prompt,
+      undefined, //for now
+      interimPulse => updateDisplayedMessage(interimPulse, beSilent, interaction)
+    )
+
+  updateDisplayedMessage(finalPulse, beSilent, interaction)
 }
 
 @Discord()
 export class Dream {
+  @ButtonComponent({ id: 'redo' })
+  async buttonHandle(interaction: ButtonInteraction): Promise<void> {
+    return
+  }
+
   @Slash({
     description: t('dream.description', Locale.EnglishGB),
     name: 'dream',
@@ -85,20 +184,11 @@ export class Dream {
     beSilent: boolean | undefined,
     interaction: CommandInteraction
   ): Promise<void> {
-    const paramsWrapper =
-      beSilent === true || beSilent === undefined ? silent({}) : {}
+    const paramsWrapper = beSilent === true ? silent({}) : {}
 
     await interaction.deferReply({ ...paramsWrapper })
 
-    const finalPulse =
-      await Dreamer.dream(
-        style,
-        prompt,
-        undefined, //for now
-        interimPulse => updateDisplayedImage(interimPulse, paramsWrapper, interaction)
-      )
-
-    updateDisplayedImage(finalPulse, paramsWrapper, interaction)
+    await processOne(prompt, style, beSilent, interaction)
 
     return
   }
